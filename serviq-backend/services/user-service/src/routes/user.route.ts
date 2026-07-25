ac4664c521639ab944a4bd9db0841949057fcd02
AryanCodeWizard <contact.aryan001@gmail.com>
2026-07-26 01:54:38 +0530

    feat(user-routes): add get-profile-details, update-profile-details and become-worker routes

--- serviq-backend/services/user-service/src/routes/user.route.ts
+++ serviq-backend/services/user-service/src/routes/user.route.ts
@@ -1,7 +1,11 @@
 import express from "express";
 const router = express.Router();
-import {createUserProfile} from '../controllers/user.controller'
+import {createUserProfile, getProfileDetails,updateProfileDetails ,becomeWorker} from '../controllers/user.controller'
 
 
 router.post("/create-profile",createUserProfile)
+router.get("/get-profile-details",getProfileDetails);
+router.put("/update-profile-details",updateProfileDetails );
+router.post("/become-worker",becomeWorker)
+
 export default router;
