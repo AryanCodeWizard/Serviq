769976c549b239b5187e74d783aa85deeaee1f3d
AryanCodeWizard <contact.aryan001@gmail.com>
2026-07-27 22:46:17 +0530

    feat(user-service): register worker verification and rejection routes

--- serviq-backend/services/user-service/src/routes/user.route.ts
+++ serviq-backend/services/user-service/src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import {createUserProfile, getProfileDetails,updateProfileDetails ,becomeWorker,addressUpdateController,getAllWorkersForVerification,getSingleWorkerForVerification,becomeAdmin} from '../controllers/user.controller'
+import {createUserProfile, getProfileDetails,updateProfileDetails ,becomeWorker,addressUpdateController,getAllWorkersForVerification,wokerVerficationReject,getSingleWorkerForVerification,becomeAdmin, wokerVerfication} from '../controllers/user.controller'
 
 
 router.post("/create-profile",createUserProfile)
@@ -13,5 +13,7 @@ router.get("/getallworker-verification",getAllWorkersForVerification);
 router.get("/getworker-verification/:id",getSingleWorkerForVerification);
 router.get("/getwoker-verification/:id",getSingleWorkerForVerification);
 router.post("/become-admin",becomeAdmin);
+router.post("/worker-verfication/:id", wokerVerfication);
+router.post("/worker-verfication-reject/:id",wokerVerficationReject)
 
 export default router;
